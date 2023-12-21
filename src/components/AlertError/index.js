612822e585a50cec15e5479fb04a92ab52bb1434
cuong_nl <cuonglc@vtdglobal.com>
2023-12-21 17:06:56 +0700

    update ui

--- src/components/AlertError/index.js
+++ src/components/AlertError/index.js
@@ -5,13 +5,18 @@ import { PiWarningCircleBold } from "react-icons/pi";
 const AlertError = ({
   text,
   style = {},
+  showIcon = true,
   iconSize = 20,
   iconColor = "white",
+  textSize = "0.9rem",
+  textPadding
 }) => {
   return (
     <div className={styles.warningBox} style={style}>
-      <PiWarningCircleBold size={iconSize} color={iconColor} />
-      <p className={styles.warningText}>{text}</p>
+      {
+        showIcon && <PiWarningCircleBold size={iconSize} color={iconColor} />
+      }
+      <p className={styles.warningText} style={{ fontSize: textSize}}>{text}</p>
     </div>
   );
 };
