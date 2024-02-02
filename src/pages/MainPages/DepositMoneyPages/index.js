7570186e202f40cdec29166aaf10056126ebb3b8
cuong_nl <cuonglc@vtdglobal.com>
2024-02-02 08:23:58 +0700

    update something =))

--- src/pages/MainPages/DepositMoneyPages/index.js
+++ src/pages/MainPages/DepositMoneyPages/index.js
@@ -1,10 +1,33 @@
 import React from "react";
 import styles from "./deposit_money.module.scss";
+import { NavLink } from "react-router-dom";
+import backgroundImage from "../../../assets/imgs/dark_low_poly_background.jpg";
 
 const DepositMoneyPages = () => {
+
+  const NavLinkItem = (path, text) => {
+    return (
+      <NavLink to={path} className={styles.navLink}>
+        {({ isActive, isPending }) => (
+          <span
+            className={styles.buttonWayra}
+            autoCorrect="false"
+          >
+            {text}
+          </span>
+        )}
+      </NavLink>
+    );
+  };
+
   return (
     <div id={styles.root}>
-      <img src={"https://img.freepik.com/free-vector/dark-low-poly-background_1048-7971.jpg?w=1800&t=st=1703950837~exp=1703951437~hmac=9be66bc99bdfed8399ea6b6846ffc42a12a0debe545d8daeda1da5c53a6a202e"} />
+      <img className={styles.background} src={backgroundImage} />
+      <nav className={styles.navContainer}>
+        {
+          NavLinkItem("/deposit-money", "Deposit Money")
+        }
+      </nav>
     </div>
   );
 };
