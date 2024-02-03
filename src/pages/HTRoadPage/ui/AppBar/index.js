8f63bc7d1ee889f19b36ce1f2c41617bcac8e97d
cuongnl <luongcuong244@gmail.com>
2024-02-04 02:00:38 +0700

    update

--- src/pages/HTRoadPage/ui/AppBar/index.js
+++ src/pages/HTRoadPage/ui/AppBar/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./app_bar.module.scss";
+import AvatarAndCoin from "./AvatarAndCoin";
 
 const AppBar = () => {
     return (
         <div id={styles.root}>
-            
+            <div className={styles.title}/>
+            <AvatarAndCoin />
         </div>
     );
 }
