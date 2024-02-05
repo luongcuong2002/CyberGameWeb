76602dcd39a721ca37e90bee6bb14befdd20f33a
cuong_nl <luongcuong244@gmail.com>
2024-02-05 23:42:17 +0700

    update

--- src/parts/LuckyWheelPopup/Wheel/index.js
+++ src/parts/LuckyWheelPopup/Wheel/index.js
@@ -1,101 +1,104 @@
-import React, { useState } from "react";
+import React, { useState, forwardRef, useImperativeHandle } from "react";
 import styles from "./wheel.module.scss";
 import LuckyWheelBackground from "../../../assets/imgs/bg_lucky_wheel.png";
 import WheelNeedle from "../../../assets/imgs/wheel_needle.png";
 import Converter from "../../../utils/converter";
 import CoinIcon from "../../../assets/imgs/img_one_coin.png";
 
-const LuckyWheelPopup = () => {
+const Wheel = forwardRef(
+    (props, ref) => {
+        
+        const wheelContent = [
+            100,
+            200,
+            500,
+            1000,
+            5000,
+            10000,
+            20000,
+            50000,
+        ]
 
-    const wheelContent = [
-        100,
-        200,
-        500,
-        1000,
-        5000,
-        10000,
-        20000,
-        50000,
-    ]
+        const minLoops = 4;
+        const spinDuration = 3000;
 
-    const minLoops = 4;
-    const spinDuration = 5000;
+        const [spinning, setSpinning] = useState(false);
 
-    const [spinning, setSpinning] = useState(false);
+        useImperativeHandle(ref, () => ({
+            async spin () {
+                if (spinning) {
+                    console.log("Spinning already");
+                    return;
+                }
 
-    const spin = async () => {
-        if (spinning) {
-            console.log("Spinning already");
-            return;
-        }
+                setSpinning(true);
 
-        setSpinning(true);
+                const wheel = document.querySelector(`.${styles.wheel}`);
 
-        const wheel = document.querySelector(`.${styles.wheel}`);
-
-        wheel.style.transition = "none";
-
-        setTimeout(() => {
-
-            const targetIndex = Math.floor(Math.random() * wheelContent.length);
-            console.log("Target index", targetIndex);
-
-            const finalDeg = targetIndex >= 0
-                ? Math.floor(360 * minLoops + (360 - targetIndex * 360 / wheelContent.length))
-                : 0;
-
-            wheel.style.transition = `all ${spinDuration / 1000}s ease-in-out`;
-            wheel.style.transform = `rotate(${finalDeg}deg)`;
-
-            setTimeout(() => {
                 wheel.style.transition = "none";
-                wheel.style.transform = `rotate(${finalDeg % 360}deg)`;
-                setSpinning(false);
 
-                alert(`You won ${Converter.formatCurrency(wheelContent[targetIndex])}`);
+                setTimeout(() => {
 
-            }, spinDuration);
+                    const targetIndex = Math.floor(Math.random() * wheelContent.length);
+                    console.log("Target index", targetIndex);
 
-        }, 500);
-    };
+                    const finalDeg = targetIndex >= 0
+                        ? Math.floor(360 * minLoops + (360 - targetIndex * 360 / wheelContent.length))
+                        : 0;
 
-    return (
-        <div className={styles.container}>
-            <img 
-                src={WheelNeedle}
-                className={styles.wheelNeedle}
-                onClick={spin} />
-            <div 
-                style={{
-                    "--seg": wheelContent.length
-                }}
-                className={styles.wheel}
-            >
-                <img src={LuckyWheelBackground} draggable="false" />
-                {
-                    wheelContent.map((amount, i) => (
-                        <div
-                            key={i}
-                            className={styles.number}
-                            style={{
-                                "--i": i,
-                                "--seg": wheelContent.length
-                            }}
-                        >
-                            <span
+                    wheel.style.transition = `all ${spinDuration / 1000}s ease-in-out`;
+                    wheel.style.transform = `rotate(${finalDeg}deg)`;
+
+                    setTimeout(() => {
+                        wheel.style.transition = "none";
+                        wheel.style.transform = `rotate(${finalDeg % 360}deg)`;
+                        setSpinning(false);
+
+                        alert(`You won ${Converter.formatCurrency(wheelContent[targetIndex])}`);
+
+                    }, spinDuration);
+
+                }, 500);
+            }
+        }));
+
+        return (
+            <div className={styles.container}>
+                <img
+                    src={WheelNeedle}
+                    className={styles.wheelNeedle} />
+                <div
+                    style={{
+                        "--seg": wheelContent.length
+                    }}
+                    className={styles.wheel}
+                >
+                    <img src={LuckyWheelBackground} draggable="false" />
+                    {
+                        wheelContent.map((amount, i) => (
+                            <div
+                                key={i}
+                                className={styles.number}
                                 style={{
+                                    "--i": i,
                                     "--seg": wheelContent.length
                                 }}
                             >
-                                {Converter.formatCurrency(amount)}
-                                <img src={CoinIcon} />
-                            </span>
-                        </div>
-                    ))
-                }
+                                <span
+                                    style={{
+                                        "--seg": wheelContent.length
+                                    }}
+                                >
+                                    {Converter.formatCurrency(amount)}
+                                    <img src={CoinIcon} />
+                                </span>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
+)
 
-export default LuckyWheelPopup;
+export default Wheel;
--- src/parts/LuckyWheelPopup/index.js
+++ src/parts/LuckyWheelPopup/index.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./lucky_wheel_popup.module.scss";
 import Popup from "reactjs-popup";
 import Wheel from "./Wheel";
+import FloatingButton from "../../components/FloatingButton";
 
 const LuckyWheelPopup = ({ setShowDialog }) => {
 
+    const wheelRef = useRef();
+
     const closeDialog = () => {
         setShowDialog(false);
+    }
+
+    const spin = () => {
+        console.log(wheelRef.current);
+        wheelRef.current?.spin();
     }
 
     return (
@@ -18,7 +26,25 @@ const LuckyWheelPopup = ({ setShowDialog }) => {
             closeOnDocumentClick={false}
         >
             <div className={styles.container}>
-                <Wheel />
+                <Wheel ref={wheelRef} />
+                <span className={styles.buttons}>
+                    <FloatingButton
+                        onClick={closeDialog}
+                        text={"Đóng"}
+                        startColor="#ACACAC"
+                        endColor="#969696"
+                        colorLayer1="#656565"
+                        colorLayer2="#D4D3D3"
+                    />
+                    <FloatingButton
+                        onClick={spin}
+                        text={"Quay"}
+                        startColor="#31D9FD"
+                        endColor="#30A7F6"
+                        colorLayer1="#2F67B4"
+                        colorLayer2="#91FCFF"
+                    />
+                </span>
             </div>
         </Popup>
     );
